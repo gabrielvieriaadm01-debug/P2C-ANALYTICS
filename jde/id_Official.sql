0f92ca307c2d2aa5997f6b0b5a650e016a9d58fe
gabrielvieriaadm01-debug <gabrielvieriaadm01@gmail.com>
2026-04-29 11:07:54 -0300

    Update id_Official.sql

--- jde/id_Official.sql
+++ jde/id_Official.sql
@@ -1,26 +1,63 @@
 SELECT
-    FHBNNF,    -- Número da Nota Fiscal
-    FHBSER,    -- Série da Nota Fiscal
-    FHMCU,     -- Centro de Custo / Filial
-    FHCO,      -- Empresa
-    FHFCO,     -- Empresa Fiscal
-    FHSHAN,    -- Address Number do Destinatário
-    FHAN8,     -- Address Number do Emitente
-    FHBCGT,    -- CNPJ/CPF
-    FHSHST,    -- Estado do Destinatário
-    FHBCGF,    -- Inscrição Estadual
-    FHISSU,    -- Data de Emissão da NF
-    FHFRTH,    -- Frete
-    FHLICP,    -- Chave de Acesso / DANFE
-    FHSTCD,    -- Status Code
-    FHUSER,    -- Usuário
-    FHPID,     -- Program ID
-    FHUPMJ,    -- Data da Última Atualização (Juliano)
-    FHTDAY,    -- Hora da Última Atualização
-    FHUSB1     -- Campo Customizado
+    FHBNNF  AS "Número da Nota Fiscal",
+    FHBSER  AS "Série da Nota Fiscal",
+    FHMCU   AS "Centro de Custo / Filial",
+    FHCO    AS "Empresa",
+    FHFCO   AS "Empresa Fiscal",
+    FHSHAN  AS "Address Number do Destinatário",
+    FHAN8   AS "Address Number do Emitente",
+    FHBCGT  AS "CNPJ/CPF",
+    FHSHST  AS "Estado do Destinatário",
+    FHBCGF  AS "Inscrição Estadual",
+
+    -- Data de Emissão
+    CONVERT(VARCHAR(10),
+        DATEADD(DAY, CAST(RIGHT(CAST(FHISSU AS VARCHAR),3) AS INT) - 1,
+            DATEFROMPARTS(
+                CASE 
+                    WHEN LEFT(CAST(FHISSU AS VARCHAR),1) = '1' THEN 2000 
+                    ELSE 1900 
+                END 
+                + CAST(SUBSTRING(CAST(FHISSU AS VARCHAR),2,2) AS INT),
+                1, 1
+            )
+        ), 103
+    ) AS "Data de Emissão da NF",
+
+    FHFRTH  AS "Frete",
+    FHLICP  AS "Chave de Acesso / DANFE",
+    FHSTCD  AS "Status Code",
+
+    -- Usuário (ID)
+    FHUSER  AS "User ID",
+
+    FHPID   AS "Program ID",
+
+    -- Data Última Atualização
+    CONVERT(VARCHAR(10),
+        DATEADD(DAY,
+            CAST(RIGHT(CAST(FHUPMJ AS VARCHAR),3) AS INT) - 1,
+            DATEFROMPARTS(
+                CASE 
+                    WHEN LEFT(CAST(FHUPMJ AS VARCHAR),1) = '1' THEN 2000 
+                    ELSE 1900 
+                END 
+                + CAST(SUBSTRING(CAST(FHUPMJ AS VARCHAR),2,2) AS INT),
+                1, 1
+            )
+        ), 103
+    ) AS "Data Última Atualização",
+
+    -- Hora Última Atualização
+    CONVERT(VARCHAR(8),
+        DATEADD(SECOND, CAST(FHTDAY AS INT), '00:00:00'),
+        108
+    ) AS "Hora Última Atualização",
+
+    FHUSB1  AS "Campo Customizado"
 
 FROM
     PRODDTA.F7601B
 
 WHERE
-    FHUPMJ BETWEEN 126001 AND 126116;
+    FHUPMJ BETWEEN 125001 AND 126116;
